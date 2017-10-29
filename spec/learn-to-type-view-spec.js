545256b0b32cefbc5c4335af6898cc9736e4ea72
berlinquin <berlinquin@gmail.com>
2017-10-28 22:35:13 -0400

    Initial commit

---
+++ spec/learn-to-type-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LearnToTypeView from '../lib/learn-to-type-view';
+
+describe('LearnToTypeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
